c69578125976622d58e6912facad20296d7049e2
gowrims <gowrishankar130496@gmail.com>
2024-10-24 21:37:47 +0530

    new style and contact save progrss on going

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function mouseUp() {
     document.getElementById("myname").textContent = "Gowri Shankar";
     document.getElementById("language").value = "தமிழ்";
 }
+
+function SaveContact(){
+    var name = document.getElementById("entername").value;
+    var email = document.getElementById("enteremail").value;
+    var textareas = document.getElementById("entertext").value;
+}
